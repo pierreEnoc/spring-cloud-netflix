452e4af7f30378909d151371c8444f41825f4247
Pierre enoc <pierre.enoc@gympass.com>
2021-02-14 11:00:08 -0300

    ExceptionHandler, ExceptionResponse

--- src/main/java/br/com/pierredev/crud/controller/ProdutoController.java
+++ src/main/java/br/com/pierredev/crud/controller/ProdutoController.java
@@ -14,8 +14,12 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +41,7 @@ public class ProdutoController {
 		this.assembler = assembler;
 	}
 	
-	@GetMapping(value = "/{id}", produces = {"application/joson", "application/xml", "application/x-yaml",})
+	@GetMapping(value = "/{id}", produces = {"application/joson", "application/xml", "application/x-yaml"})
 	public ProdutoVO findById(@PathVariable("id") Long id) {
 		ProdutoVO produtoVO = produtoService.findById(id);
 		produtoVO.add(linkTo(methodOn(ProdutoController.class).findById(id)).withSelfRel());
@@ -46,7 +50,7 @@ public class ProdutoController {
 	}
 	
 
-	@GetMapping(value = "/{id}", produces = {"application/joson", "application/xml", "application/x-yaml",})
+	@GetMapping(value = "/{id}", produces = {"application/joson", "application/xml", "application/x-yaml"})
 	public  ResponseEntity<?> findByIdAll(@RequestParam(value = "page", defaultValue = "0") int page,
 			@RequestParam(value = "limit", defaultValue = "0") int limit,
 			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
@@ -64,6 +68,30 @@ public class ProdutoController {
 		return new ResponseEntity<>(pagedModel, HttpStatus.OK);
 	}
 	
+	@PostMapping(produces = {"application/joson", "application/xml", "application/x-yaml"},
+			     consumes = {"application/joson", "application/xml", "application/x-yaml"})
+	public ProdutoVO create(@RequestBody ProdutoVO produtoVO) {
+		ProdutoVO proVo = produtoService.create(produtoVO);
+		produtoVO.add(linkTo(methodOn(ProdutoController.class).findById(produtoVO.getId())).withSelfRel());
+		
+		return proVo;
+		
+	}
 	
-
+	@PutMapping(produces = {"application/joson", "application/xml", "application/x-yaml"},
+		     consumes = {"application/joson", "application/xml", "application/x-yaml"})
+public ProdutoVO upadte(@RequestBody ProdutoVO produtoVO) {
+	ProdutoVO proVo = produtoService.update(produtoVO);
+	produtoVO.add(linkTo(methodOn(ProdutoController.class).findById(produtoVO.getId())).withSelfRel());
+	
+	return proVo;
+	
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+		produtoService.delete(id);
+		return ResponseEntity.ok().build();
+	}
+	
 }
