a85680ba5ede0db6926f9a1b5081013f44e1297e
Pierre enoc <pierre.enoc@gympass.com>
2021-02-14 10:09:08 -0300

    findAll

--- src/main/java/br/com/pierredev/crud/controller/ProdutoController.java
+++ src/main/java/br/com/pierredev/crud/controller/ProdutoController.java
@@ -1,17 +1,27 @@
 package br.com.pierredev.crud.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.pierredev.crud.data.vo.ProdutoVO;
 import br.com.pierredev.crud.services.ProdutoService;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/produto")
@@ -30,9 +40,28 @@ public class ProdutoController {
 	@GetMapping(value = "/{id}", produces = {"application/joson", "application/xml", "application/x-yaml",})
 	public ProdutoVO findById(@PathVariable("id") Long id) {
 		ProdutoVO produtoVO = produtoService.findById(id);
-		produtoVO.add(linkTo());
+		produtoVO.add(linkTo(methodOn(ProdutoController.class).findById(id)).withSelfRel());
 		
-		return null;
+		return produtoVO;
+	}
+	
+
+	@GetMapping(value = "/{id}", produces = {"application/joson", "application/xml", "application/x-yaml",})
+	public  ResponseEntity<?> findByIdAll(@RequestParam(value = "page", defaultValue = "0") int page,
+			@RequestParam(value = "limit", defaultValue = "0") int limit,
+			@RequestParam(value = "direction", defaultValue = "asc") String direction) {
+		//if ternal
+		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
+		
+		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "nome"));
+		
+		Page<ProdutoVO> produtos = produtoService.findAll(pageable);
+		produtos.stream()
+			.forEach(p -> p.add(linkTo(methodOn(ProdutoController.class).findById(p.getId())).withSelfRel()));
+		
+		PagedModel<EntityModel<ProdutoVO>> pagedModel = assembler.toModel(produtos);
+		
+		return new ResponseEntity<>(pagedModel, HttpStatus.OK);
 	}
 	
 	
