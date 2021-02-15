707b4c7b0fffe651aa6d2361a95c27ab34cf5bc7
Pierre enoc <pierre.enoc@gympass.com>
2021-02-15 19:37:23 -0300

    add queu for service

--- src/main/java/br/com/pierredev/crud/services/ProdutoService.java
+++ src/main/java/br/com/pierredev/crud/services/ProdutoService.java
@@ -2,6 +2,7 @@ package br.com.pierredev.crud.services;
 
 import java.util.Optional;
 
+import br.com.pierredev.crud.message.ProdutoSendMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -16,15 +17,17 @@ import br.com.pierredev.crud.repositories.ProdutoRepository;
 public class ProdutoService {
 	
 	private final ProdutoRepository produtoRepository;
+	private final ProdutoSendMessage produtoSendMessage;
 	
 	@Autowired
-	public ProdutoService(ProdutoRepository produtoRepository) {
-		super();
+	public ProdutoService(ProdutoRepository produtoRepository, ProdutoSendMessage produtoSendMessage) {
 		this.produtoRepository = produtoRepository;
+		this.produtoSendMessage = produtoSendMessage;
 	}
 	
 	public ProdutoVO create(ProdutoVO produtoV0) {
 		ProdutoVO proVoRetorno = ProdutoVO.create(produtoRepository.save(Produto.create(produtoV0)));
+		produtoSendMessage.sendMessage(produtoV0);
 		return proVoRetorno;
 	}
 	
